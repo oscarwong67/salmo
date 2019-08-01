fe62d855d99bb71c0d2445eea88ed86ce848ea71
Oscar Wong <wong.oscar67@gmail.com>
2019-07-31 21:40:11 -0700

    successfully scraped the jobs - have not yet done anything to plot the data and visualize it with d3 yet tho

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from scrape import scrape_indeed_feed
 from scrape import scrape_indeed_job_page
+from collections import Counter
 
-NUM_PAGES_TO_SCRAPE = 2
+NUM_PAGES_TO_SCRAPE = 10
 
 def scrape_indeed(url):
   job_urls = []
   for i in range(0, NUM_PAGES_TO_SCRAPE):
     scrape_indeed_feed(url, i, job_urls)
   
+  job_words = []
   for job_url in job_urls:
-    scrape_indeed_job_page(job_url)
+    scrape_indeed_job_page(job_url, job_words)
+  counts = Counter(job_words)
+  print(counts.most_common(100))
   
 # Indeed.ca "software engineering internship" - first page, filtered by internship, no location
-scrape_indeed('https://www.indeed.ca/jobs?q=software+engineering+internship&jt=internship')
+# scrape_indeed('https://www.indeed.ca/jobs?q=software+engineering+internship&jt=internship')
+
+# Indeed.com "software engineering internship" - first page, filtered by internship, no location
+scrape_indeed('https://www.indeed.com/jobs?q=software%20engineering%20internship&jt=internship&vjk=a053bc5378958f13')
--- scrape.py
+++ scrape.py
@@ -16,42 +16,27 @@ def scrape_indeed_feed(url, page, job_urls):
   soup = BeautifulSoup(raw_html, 'html.parser')
   get_job_postings(soup, job_urls)
 
-def clean_indeed_job_page(url): #  https://jessesw.com/Data-Science-Skills/
+def create_list_of_words(bullets, bullet_words):
+  for bullet in bullets:
+    text = bullet.get_text()
+
+    # clean up text, from https://jessesw.com/Data-Science-Skills/    
+    text = re.sub("[^a-zA-Z.+3]"," ", text) # Now get rid of any terms that aren't words (include 3 for d3.js)
+                                            # Also include + for C++
+        
+    text = text.lower().split()  # Go to lower case and split apart
+        
+    stop_words = set(stopwords.words("english")) # Filter out any stop words
+    text = [w for w in text if not w in stop_words]
+
+    text = list(set(text)) # Last, just get the set of these. Ignore counts (we are just looking at whether a term existed
+                            # or not on the website)
+    bullet_words.extend(text)
+
+def scrape_indeed_job_page(url, job_words):  
   raw_html = simple_get(url)
   soup = BeautifulSoup(raw_html, 'html.parser')
-
-  for script in soup(["script", "style"]):
-        script.extract() # Remove these two elements from the BS4 object
-    
-  page_text = soup.get_text() # Get the page_text from this   
-  
-  lines = (line.strip() for line in page_text.splitlines()) # break into lines 
- 
-  chunks = (phrase.strip() for line in lines for phrase in line.split("  ")) # break multi-headlines into a line each
-  
-  def chunk_space(chunk):
-      chunk_out = chunk + ' ' # Need to fix spacing issue
-      return chunk_out  
-      
-  page_text = ''.join(chunk_space(chunk) for chunk in chunks if chunk).encode('utf-8') # Get rid of all blank lines and ends of line
-      
-  # Now clean out all of the unicode junk (this line works great!!! 
-  try:
-      page_text = page_text.decode('unicode_escape') # Need this as some websites aren't formatted
-  except:                                  # in a way that this works, can occasionally throw
-      return                               # an exception
-         
-  page_text = re.sub("[^a-zA-Z.+3]"," ", page_text)  # Now get rid of any terms that aren't words (include 3 for d3.js)
-                                           # Also include + for C++
-  page_text = page_text.lower().split()  # Go to lower case and split them apart
-          
-  stop_words = set(stopwords.words("english")) # Filter out any stop words
-  page_text = [w for w in page_text if not w in stop_words]
-      
-  page_text = list(set(page_text)) # Last, just get the set of these. Ignore counts (we are just looking at whether a term existed
-                          # or not on the website)
-  return page_text
-
-def scrape_indeed_job_page(url):
-  page_text = clean_indeed_job_page(url)
-  print(page_text[:20])
+  bullets = soup.find_all('li')
+  bullet_words = []
+  create_list_of_words(bullets, bullet_words)
+  job_words.extend(bullet_words)
